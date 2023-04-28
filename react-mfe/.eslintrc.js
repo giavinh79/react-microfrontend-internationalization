e0868a89818c5d75e5c391d036ebae9c143cfaaf
giavinh79 <giavinhlam@gmail.com>
2023-04-28 16:46:05 -0400

    add linting for translation files

--- react-mfe/.eslintrc.js
+++ react-mfe/.eslintrc.js
@@ -1,10 +1,18 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const path = require('path');
+
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:i18n-json/recommended',
+  ],
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -15,5 +23,15 @@ module.exports = {
   rules: {
     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
     'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    'i18n-json/identical-keys': [
+      2,
+      {
+        filePath: {
+          'error.json': path.resolve('./public/locales/en/error.json'),
+          'mfe.json': path.resolve('./public/locales/en/mfe.json'),
+        },
+      },
+    ],
+    'i18n-json/valid-message-syntax': 0,
   },
 };
